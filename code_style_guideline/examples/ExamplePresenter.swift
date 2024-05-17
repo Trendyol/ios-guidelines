85b6b06d82b904bf82b3ee7e4aa937d0b79c4705
Emin Özkalaycıoğlu <emin.ozkalaycioglu@dolap.com>
2024-05-17 11:59:03 +0300

    add additions and example presenter

---
+++ code_style_guideline/examples/ExamplePresenter.swift
@@ -0,0 +1,151 @@
+import CommonKit
+import CoreTracker
+import CoreUtils
+import DependencyEngine
+import MemberKit
+
+protocol ExamplePresenterInterface { 
+    var shouldShowButton: Bool { get }
+    var fullName: String { get }
+    var shouldShowXComponent: Bool { get }
+
+    func firstMethod() -> Bool
+    func secondMethod()
+}
+
+private extension ExamplePresenter {
+    enum Constants {
+        static let dataFetchLimit = 2
+        static let defaultBarTitle = "..."
+    }
+}
+
+final class ExamplePresenter {
+    private typealias View = ExampleViewInterface
+    private typealias Interactor = ExampleInteractorInterface
+
+    static let staticProperty: Int = 1
+    class var classProperty: String = "Trendyol"
+
+    private weak var view: View?
+    private let interactor: Interactor
+    private let userId: Int
+    private lazy var manager = FavoriteManager()
+
+    // MARK: - Identitiy Properties
+    private var name: String
+    private var surname: String
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+
+    // MARK: - X Component Properties
+    private var xComponentTitle: String
+    private var isXComponentFlagEnabled: Bool
+    var shouldShowXComponent: Bool {
+        !xComponentTitle.isEmpty && isXComponentFlagEnabled
+    }
+
+    var shouldShowButton: Bool {
+        firstFlag && secondFlag
+    }
+
+    var barTitle: String {
+        data.productName ?? Constants.defaultBarTitle
+    }
+
+    init(view: View, interactor: Interactor, userId: Int) {
+        self.view = view
+        self.interactor = interactor
+        self.userId = userId
+    }
+    
+    static func workHard(motivation: Bool = true) {
+        .
+        .
+    }
+
+    class func beHappy() {
+        .
+        .
+    }
+}
+
+private extension ExamplePresenter {
+    func fetchFirstData() {
+        .
+        .
+        interactor.fetchFirstData()
+        .
+        .
+    }
+
+    func fetchSecondData() {
+        .
+        .
+        interactor.fetchSecondData()
+        .
+        .
+    }
+
+    @discardableResult
+    func handleName(with text: String?) -> Bool {
+        guard let text else { return false }
+        manager.name = text
+        secondMethod()
+        return true
+    }
+}
+
+// MARK: - ExamplePresenterInterface
+extension ExamplePresenter: ExamplePresenterInterface {
+    func firstMethod() -> Bool {
+        .
+        .
+        .
+        return result
+    }
+
+    func secondMethod() {
+        guard isXComponentFlagEnabled else { return }
+        .
+        .
+        .
+        .
+        if firstCondition {
+            .
+            .
+        } else if secondCondition {
+            .
+        } else {
+            .
+            .
+        }
+        .
+        .
+    }
+}
+
+// MARK: - TYPopupDelegate
+extension ExamplePresenter: TYPopupDelegate {
+    func tyPopup(_ popup: TYPopup, didClickButtonFor type: Type) {
+        .
+        .
+    }
+}
+
+extension ExamplePresenter: FavoriteManagerDelegate {
+    func favoriteManagerDidAddToListSuccessfully(_ manager: FavoriteManager) {
+        .
+        .
+        .
+    }
+}
+
+// MARK: - ExampleInteractorOutput
+extension ExamplePresenter: ExampleInteractorOutput {
+    func handleResult(_ result: Result<ExampleResponseModel, Error>) {
+        .
+        .
+    }
+}
